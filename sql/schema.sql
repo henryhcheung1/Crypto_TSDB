2aa47fad758b3cfb774d92ffc9945c86b1f8f159
henryhcheung1 <henryhcheung1@gmail.com>
2021-11-20 01:21:59 -0800

    Adjust schema and push empty dockerfile

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,21 +1,22 @@
+CREATE SCHEMA crypto;
+
 --Schema for cryptocurrency analysis
-DROP TABLE IF EXISTS "currency_info";
-CREATE TABLE "currency_info"(
+DROP TABLE IF EXISTS "crypto.currency_info";
+CREATE TABLE "crypto.currency_info"(
    currency_code   VARCHAR (10),
    currency        TEXT
 );
 
 --Schema for crypto_prices table
-DROP TABLE IF EXISTS "crypto_prices";
-CREATE TABLE "crypto_prices"(
+DROP TABLE IF EXISTS "crypto.crypto_prices";
+CREATE TABLE "crypto.crypto_prices"(
    time            TIMESTAMP WITH TIME ZONE NOT NULL,
    opening_price   DOUBLE PRECISION,
    highest_price   DOUBLE PRECISION,
    lowest_price    DOUBLE PRECISION,
    closing_price   DOUBLE PRECISION,
    volume_crypto   DOUBLE PRECISION,
-   volume_btc      DOUBLE PRECISION,
    currency_code   VARCHAR (10)
 );
 
-SELECT create_hypertable('crypto_prices', 'time');
+SELECT create_hypertable('crypto.crypto_prices', 'time');
